444eeb9cb6bedf24457d7c4eead42595bc0ee85b
pgm95 <131979102+pgm95@users.noreply.github.com>
2026-07-28 16:42:22 -0500

    fix(quantum): trust forwarded proxy headers for OIDC redirects
    Quantum 2.x no longer honors X-Forwarded-Proto by default, so the OIDC redirect_uri was built as http and failed Authentik's matching. Solution: Trust X-Forwarded-Proto and X-Forwarded-Host in http config
    - Set externalUrl for share links with DOMAIN_PRIVATE
    - Other config streamlining changes

--- stacks/apps/quantum/config/init.sh
+++ stacks/apps/quantum/config/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
-sed "s/\${DOMAIN_PUBLIC}/${DOMAIN_PUBLIC}/g" /tmp/config.yaml.tpl > "${FILEBROWSER_CONFIG}"
+sed -e "s/\${DOMAIN_PUBLIC}/${DOMAIN_PUBLIC}/g" \
+    -e "s/\${DOMAIN_PRIVATE}/${DOMAIN_PRIVATE}/g" /tmp/config.yaml.tpl > "${FILEBROWSER_CONFIG}"
 
 exec ./filebrowser "$@"
